b5a97d63e45e27ff0186e1075be82d6e3745e3bf
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2020-10-28 12:05:22 -0500

    feat: added matching functionality

--- match-card-game/index.js
+++ match-card-game/index.js
@@ -1,12 +1,16 @@
 const suits = ['Hearts','Diamonds','Spades','Clubs']
 const values =  ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
 const decksInput = document.getElementById('numOfDecks')
+const matchSelect = document.getElementById('matchSelect')
 
 const createDeck = () => {
   const deck = []
   suits.forEach((suit)=>{
     values.forEach((value)=>{
-      deck.push(`${value} of ${suit}`)
+      deck.push({
+        suit,
+        value
+      })
     })
   })
   return deck
@@ -33,13 +37,44 @@ const shuffleDeck = (deck) => {
     deck[counter] = deck[index]
     deck[index] = temp
   }
-
   return deck
+}
+
+const isMatch = (card1, card2) => {
+  switch(matchSelect.value){
+    case 'suit':
+      if(card1.suit === card2.suit){
+        return true
+      }else{
+        return false
+      }
+    case 'value':
+      if(card1.value === card2.value){
+        return true
+      }else{
+        return false
+      }
+    case 'both':
+      if((card1.value === card2.value)&&(card1.suit === card2.suit)){
+        return true
+      }else{
+        return false
+      }
+  }
 }
 
 const handleButton = () => {
   const myDeck = shuffleDeck(combineDecks(decksInput.value))
   decksInput.value = ''
-  console.log(myDeck)
 }
+
+// const exampleCard1 = {
+//   suit: 'Hearts',
+//   value: 'Ace'
+// }
+
+// const exampleCard2 = {
+//   suit:'Hearts',
+//   value: '2'
+// }
 
